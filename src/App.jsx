9e7bde80ae2075d9f3c2b57719596f392f213e5c
25132563 <samara_rolim010@hotmail.com>
2026-04-17 11:26:53 -0300

    começando a criação dos componentes com tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <h1 className="">Gerenciador de Tarefas</h1>
+    </div>
+  );
+}
+
+export default App;
