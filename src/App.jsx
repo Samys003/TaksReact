ceb609dab8b4948a9dfd34764091a0b46b7801fb
25132563 <samara_rolim010@hotmail.com>
2026-04-24 10:01:05 -0300

    componente de tarefas criado, iniciado a funcionalidade do mesmo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import Tasks from "./components/Tasks";
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Estudar Programação",
+      description:
+        "as 15 horas do dia 24 preciso estudar react, aprender um pouco mais sobre node",
+      isCompleted: true,
+    },
+    {
+      id: 1,
+      text: "Estudar Enem",
+      description:
+        "hoje terei que ver alguns conceitos matematicos e montar o cronograma de biologia",
+      isCompleted: true,
+    },
+    {
+      id: 1,
+      text: "dentista",
+      description: "hoje as 15 horas preciso ir ao dentista fazer uma limpeza",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // validação da atualização da tarefa
+      if (task.id === taskId)
+        return { ...task, isCompleted: !task.isCompleted };
+
+      return task;
+    });
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <h1 className="">Gerenciador de Tarefas</h1>
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 }
