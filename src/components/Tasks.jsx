b43a837c836ba1a4becf32494eb13aa08f9bdcdb
25132563 <samara_rolim010@hotmail.com>
2026-04-29 09:58:33 -0300

    correção da criação de lista com um icone de checkout

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -18,8 +18,9 @@ function Tasks( {tasks, onTaskClick, deleteOnTaskClick}) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 w-full text-white p-2 rounded-md text-left ${task.isCompleted && "line-through"}`}
+            className={`bg-slate-400 w-full text-white p-2 rounded-md text-left flex gap-2 ${task.isCompleted && "line-through"}`}
           >
+            {task.isCompleted && <CheckIcon/>}
             {task.text}
           </button>
           <Button onClick={() => onSeeDetailsClick(task)}>
